cea18d1209b3a2076e6e17ea4be45a263315964d
katoy <youichikato@gmail.com>
2020-08-23 06:57:25 +0900

    Add form for edit user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,8 +41,7 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
+        format.html { redirect_to '/users' }
       else
         format.html { render :edit }
         format.json { render json: @user.errors, status: :unprocessable_entity }
